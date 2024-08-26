405b4fffb1e34627e17c49c5d8369ad54fcdcf91
jaya surya <suryajaya4899@gmail.com>
2024-08-26 22:19:50 +0530

    url updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const url = process.env.MONGO_URI;
 
 // CORS configuration
 app.use(cors({
-    origin: 'http://localhost:3000', // Update to the correct frontend URL
+    origin: 'https://assessment-writo-education-ui.vercel.app', // Update to the correct frontend URL
     methods: ['GET', 'POST'],
     credentials: true
 }));
